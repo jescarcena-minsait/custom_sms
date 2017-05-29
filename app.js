67113601d756650b80c03c08fb0e6cbb9254dabd
Jesús <jescarcena@minsait.com>
2017-05-29 09:36:53 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,102 @@
+'use strict';
+// Module Dependencies
+// -------------------
+var express     = require('express');
+var http        = require('http');
+var JWT         = require('./lib/jwtDecoder');
+var path        = require('path');
+var request     = require('request');
+var routes      = require('./routes');
+var activity    = require('./routes/activity');
+var pkgjson = require( './package.json' );
+
+var app = express();
+
+// Register configs for the environments where the app functions
+// , these can be stored in a separate file using a module like config
+var APIKeys = {
+    appId           : 'c3e0c212-691e-4420-9b8d-b62fd8953c3f',
+    clientId        : 'jnzau01wdawz0hr2vfkcf3fw',
+    clientSecret    : 'JDPtwb76ynxQaEMpwgjCk7BI',
+    appSignature    : 'utjl5gjt4dj21ozfidsub12ry4vokd0ii20ph4ucylhahhpskc3fyqzea42veycndxscx3pies22s4lybdlz3hwtuth134ciesd0jfnw2rv1rdd3em2dwosvyohbcrdor4jeadpqiorxfg4j5tuotcy1edo2rfnxsgcuph3he2dydglrl4tzhpnpryulnuzqxu25hkwgvfiywlfjslwnsqwnasrbbsfzue2wzwbkcxejrrigu1rjrjb35hpqlkg',
+    authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
+};
+
+// Simple custom middleware
+function tokenFromJWT( req, res, next ) {
+    // Setup the signature for decoding the JWT
+    var jwt = new JWT({appSignature: APIKeys.appSignature});
+    
+    // Object representing the data in the JWT
+    var jwtData = jwt.decode( req );
+
+    // Bolt the data we need to make this call onto the session.
+    // Since the UI for this app is only used as a management console,
+    // we can get away with this. Otherwise, you should use a
+    // persistent storage system and manage tokens properly with
+    // node-fuel
+    req.session.token = jwtData.token;
+    next();
+}
+
+// Use the cookie-based session  middleware
+app.use(express.cookieParser());
+
+// TODO: MaxAge for cookie based on token exp?
+app.use(express.cookieSession({secret: "HelloWorld-CookieSecret"}));
+
+// Configure Express
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.favicon());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express in Development Mode
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// HubExchange Routes
+app.get('/', routes.index );
+app.post('/login', tokenFromJWT, routes.login );
+app.post('/logout', routes.logout );
+
+// Custom Hello World Activity Routes
+app.post('/ixn/activities/hello-world/save/', activity.save );
+app.post('/ixn/activities/hello-world/validate/', activity.validate );
+app.post('/ixn/activities/hello-world/publish/', activity.publish );
+app.post('/ixn/activities/hello-world/execute/', activity.execute );
+
+app.get('/clearList', function( req, res ) {
+	// The client makes this request to get the data
+	activity.logExecuteData = [];
+	res.send( 200 );
+});
+
+
+// Used to populate events which have reached the activity in the interaction we created
+app.get('/getActivityData', function( req, res ) {
+	// The client makes this request to get the data
+	if( !activity.logExecuteData.length ) {
+		res.send( 200, {data: null} );
+	} else {
+		res.send( 200, {data: activity.logExecuteData} );
+	}
+});
+
+app.get( '/version', function( req, res ) {
+	res.setHeader( 'content-type', 'application/json' );
+	res.send(200, JSON.stringify( {
+		version: pkgjson.version
+	} ) );
+} );
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
